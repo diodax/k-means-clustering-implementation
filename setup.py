0b320943336da18789bf1f0e023212a811890cc5
Diosdavi Lara <diosdavi@megsoftconsulting.com>
2019-03-02 20:52:53 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of a K-Means clustering algorithm to cluster crowd-acquired user stories about smart home applications.',
+    author='Diosdavi Lara',
+    license='',
+)
